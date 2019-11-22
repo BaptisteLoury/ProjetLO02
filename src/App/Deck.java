9bd231ce40ce94cca05756c41f1263ec5c63674a
BaptisteLoury <ouchibabaptiste@gmail.com>
2019-11-22 12:42:23 +0100

    Transfert vers Deck

--- src/App/Deck.java
+++ src/App/Deck.java
@@ -1,13 +1,28 @@
 package App;
+import java.util.LinkedList;;
 
 public class Deck {
 
 	private int nombreCartes;
 	
+	private LinkedList<Cartes> deckCartes;
+	
 	public Deck() {
 		
 	}
-	
+	public void creerJeuDeBase() {
+		int trophee = 0;
+		for (Couleur ind1 : Couleur.values()) {
+			for (Valeur ind2 : Valeur.values()) {
+				Cartes carte = new Cartes(EnumTrophee.values()[trophee],ind1,ind2);
+				deckCartes.add(carte);
+				trophee++;
+			}
+		}
+		Cartes joker = new Cartes(EnumTrophee.BestJest,Couleur.JOKER,Valeur.JOKER);
+		deckCartes.add(joker);
+	}
+
 	public void remplirDeck() {
 		
 	}
